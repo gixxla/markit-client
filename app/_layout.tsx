4e79832d99a46823002c9b352c6d609fe69fbb2c
gixxla <gigixquisite@gmail.com>
2025-12-20 20:12:00 +0900

    Refactor: 레이아웃 컴포넌트에 SafeAreaProvider 및 Toast 적용
    - RootLayout이 SafeAreaProvider를 사용하도록 리팩토링하고, 성능 향상을 위해 useCallback을 사용하는 방식으로 useEffect 로직 변경
    - RootLayout 및 AuthLayout에 Toast를 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,22 @@
 import "../global.css";
-import React, { useEffect } from "react";
+import React, { useCallback } from "react";
 import { Stack } from "expo-router";
+import { View } from "react-native";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { FONTS } from "constants/fonts";
+import Toast from "react-native-toast-message";
+import { toastConfig } from "../components/ToastConfig";
 
 SplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts(FONTS);
 
-  useEffect(() => {
+  const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
-      SplashScreen.hideAsync().catch(() => {});
+      await SplashScreen.hideAsync().catch(() => {});
     }
   }, [fontsLoaded]);
 
@@ -21,17 +25,22 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="(tabs)" />
+    <SafeAreaProvider>
+      <View className="flex-1" onLayout={onLayoutRootView}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
 
-      <Stack.Screen
-        name="auth"
-        options={{
-          presentation: "modal",
-          animation: "slide_from_bottom",
-        }}
-      />
-    </Stack>
+          <Stack.Screen
+            name="auth"
+            options={{
+              presentation: "modal",
+              animation: "slide_from_bottom",
+            }}
+          />
+        </Stack>
+        <Toast config={toastConfig} />
+      </View>
+    </SafeAreaProvider>
   );
 }
--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { Stack } from "expo-router";
+import { View } from "react-native";
+import Toast from "react-native-toast-message";
+import { toastConfig } from "../../components/ToastConfig";
 
 export default function AuthLayout() {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: "white" },
-      }}
-    >
-      <Stack.Screen name="signup" />
-      <Stack.Screen name="login" />
-      <Stack.Screen name="email-signup" />
-      <Stack.Screen name="email-login" />
-    </Stack>
+    <View className="flex-1">
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: "white" },
+        }}
+      >
+        <Stack.Screen name="signup" />
+        <Stack.Screen name="login" />
+        {/* 회원가입 */}
+        <Stack.Screen name="email-signup" />
+        <Stack.Screen name="email-signup-verify" />
+        <Stack.Screen name="email-signup-success" />
+        {/* 로그인 입력 */}
+        <Stack.Screen name="email-login" />
+      </Stack>
+      <Toast config={toastConfig} />
+    </View>
   );
 }
